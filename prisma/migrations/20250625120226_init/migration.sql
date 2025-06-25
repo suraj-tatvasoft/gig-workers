b4781ad3869f0a306cbc80dd256d406203142956
Parth Shobhana <parth.shobhana@tatvasoft.com>
2025-06-25 18:02:43 +0530

    removed prisma migration

--- prisma/migrations/20250625120226_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `last_name` on table `User` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "User" ALTER COLUMN "last_name" SET NOT NULL;
