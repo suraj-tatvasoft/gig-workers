0037544ba7f1d1ae4aeb764eff607daae3291337
Sachin Yadav <sachin.yadav@tatvasoft.com>
2025-07-02 16:22:58 +0530

    Sidebar Changes For Admin Panel And Dashboard

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,20 +1,16 @@
 import { BarChart3, Briefcase, Users2Icon } from 'lucide-react';
+import { PRIVATE_ROUTE } from './app-routes';
 
 export const constant = 'CONST';
 export const EMAIL_TOKEN_EXPIRY_TIME = '1h';
 export const BRAND_NAME = 'Gig Workers';
 export const BASE_API_URL = '/api';
 
-export const user_navigation_menu = [{ name: 'Dashboard', icon: BarChart3, href: '/dashboard' }];
-
-export const super_admin_navigation_menu = [
-  { name: 'Users', icon: Users2Icon, href: '/admin/dashboard/users' },
-  { name: 'Gigs', icon: Briefcase, href: '/admin/dashboard/gigs' },
+export const user_navigation_menu = [
+  { name: 'Dashboard', icon: BarChart3, href: PRIVATE_ROUTE.DASHBOARD },
 ];
 
-export const DEFAULT_PAGINATION = {
-  page: 1,
-  pageSize: 10,
-  total: 0,
-  totalPages: 0,
-};
+export const super_admin_navigation_menu = [
+  { name: 'Users', icon: Users2Icon, href: PRIVATE_ROUTE.ADMIN_USERS_DASHBOARD_PATH },
+  { name: 'Gigs', icon: Briefcase, href: PRIVATE_ROUTE.ADMIN_GIGS_DASHBOARD_PATH },
+];
