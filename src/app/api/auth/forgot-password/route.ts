a5001abfbbe921f5952f856fd0a7c771a2cb6e2a
Sachin Yadav <sachin.yadav@tatvasoft.com>
2025-07-15 12:21:40 +0530

    Reverted Code

--- src/app/api/auth/forgot-password/route.ts
+++ src/app/api/auth/forgot-password/route.ts
@@ -15,8 +15,6 @@ import {
   VERIFICATION_CODES
 } from '@/constants';
 
-export const runtime = 'nodejs';
-
 export async function POST(req: Request) {
   try {
     const body = await req.json();
--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -25,8 +25,6 @@ import {
 } from '@/constants';
 const io = getSocketServer();
 
-export const runtime = 'nodejs';
-
 export async function POST(req: Request) {
   try {
     const body = await req.json();
